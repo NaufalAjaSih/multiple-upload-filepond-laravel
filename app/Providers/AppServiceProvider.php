df494a963b0ecc9cf5d2c75f69dfa235715b229c
NaufalAjaSih <masanaufal@gmail.com>
2023-05-23 16:38:57 +0700

    first commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Book;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\ServiceProvider;
+use Inertia\Inertia;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Inertia::share([
+            'errors' => function () {
+                return Session::get('errors')
+                    ? Session::get('errors')->getBag('default')->getMessages()
+                    : (object) [];
+            },
+        ]);
+
+        Inertia::share('flash', function () {
+            return [
+                'message' => Session::get('message'),
+            ];
+        });
+
+        Inertia::share('csrf_token', function(){
+            return csrf_token();
+        });
+
+        // Book::observe(BookObserver::class);
+    }
+}
---
+++ database/seeders/BookSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Database\Factories\BookFactory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class BookSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $fact = new BookFactory();
+        $fact->count(10)->create();
+    }
+}
